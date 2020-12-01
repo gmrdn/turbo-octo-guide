d7f11b079d0f7e664e289d705dc085495ffc9cf2
Guillaume Rondon <guillaume.rondon@gmail.com>
2020-12-01 21:11:05 +0100

    Solved first day

---
+++ advent.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type ExpenseReport struct {
+	Entries []ExpenseEntry
+}
+
+type ExpenseEntry struct {
+	value int
+}
+
+func (report *ExpenseReport) AddExpense(entry ExpenseEntry) []ExpenseEntry {
+    report.Entries = append(report.Entries, entry)
+    return report.Entries
+}
+
+
+func (report *ExpenseReport) findPairMaking(targetValue int) [2]int {
+	for i:=0; i<len(report.Entries); i++{
+		indexFirstNumber := i
+		valueFirstNumber := report.Entries[i].value
+		for j := 0; j < len(report.Entries); j++ {
+			indexSecondNumber := j
+			valueSecondNumber := report.Entries[j].value
+			if indexSecondNumber != indexFirstNumber && valueFirstNumber + valueSecondNumber== targetValue {
+				return [2]int{valueFirstNumber, valueSecondNumber}
+			} 
+		}
+	}
+	return [2]int{0,0}
+}
+
+func (report *ExpenseReport) FindPairMaking2020() [2]int {
+	return report.findPairMaking(2020)
+}
+
+func (report *ExpenseReport) FindThreeMaking2020() [3]int {
+	for i:=0; i<len(report.Entries); i++ {
+		valueFirstNumber := report.Entries[i].value
+		target := 2020 - valueFirstNumber
+		twoOthers := report.findPairMaking(target)
+		if twoOthers[0] != 0 && twoOthers[1] != 0 {
+			return [3]int{valueFirstNumber, twoOthers[0], twoOthers[1]}
+		}
+	}
+	return [3]int{0,0,0}
+}
+
+
+func main()  {
+	
+	fileHandle, _ := os.Open("datainput.txt")
+	defer fileHandle.Close()
+	fileScanner := bufio.NewScanner(fileHandle)
+
+
+	report := ExpenseReport{
+		Entries: []ExpenseEntry{},
+	}
+
+	for fileScanner.Scan() {
+		if s, err := strconv.Atoi(fileScanner.Text()); err == nil {
+			report.AddExpense(ExpenseEntry{s})
+		}
+	}
+
+	pair := report.FindPairMaking2020()
+
+	fmt.Printf("First value: %d, second value: %d, product: %d\n", pair[0], pair[1], pair[0] * pair[1])
+
+	tripplet := report.FindThreeMaking2020()
+
+
+	fmt.Printf("First value: %d, second value: %d, third value: %d,  product: %d\n", tripplet[0], tripplet[1], tripplet[2], tripplet[0] * tripplet[1] * tripplet[2])
+
+}
---
+++ advent_test.go
@@ -0,0 +1,40 @@
+package main
+
+import "testing"
+
+
+func InitExpenseReport() ExpenseReport {
+	report := ExpenseReport{
+		Entries: []ExpenseEntry{},
+	}
+	report.AddExpense(ExpenseEntry{1721})
+	report.AddExpense(ExpenseEntry{979})
+	report.AddExpense(ExpenseEntry{366})
+	report.AddExpense(ExpenseEntry{299})
+	report.AddExpense(ExpenseEntry{675})
+	report.AddExpense(ExpenseEntry{1456})
+
+	return report
+}
+
+func TestFindPairMaking2020(t *testing.T) {
+
+	report := InitExpenseReport()
+
+	want := [2]int{1721,299}
+	got := report.FindPairMaking2020()
+    if got != want {
+       t.Errorf("got: %d, instead of: %d.", got, want)
+    }
+}
+
+func TestFindThreeMaking2020(t *testing.T) {
+
+	report := InitExpenseReport()
+
+	want := [3]int{979,366,675}
+	got := report.FindThreeMaking2020()
+    if got != want {
+       t.Errorf("got: %d, instead of: %d.", got, want)
+    }
+}
