67bf8e85613b16371a611322643be9ccbe333dc8
Guillaume Rondon <gr@prixtel.com>
2020-12-03 17:46:16 +0100

    Day 3 part 2

--- src/tobogggan_trajectory/toboggan.go
+++ src/tobogggan_trajectory/toboggan.go
@@ -47,8 +47,11 @@ func (area *Area) HowManyTrees(slope Slope) int {
 	nb := 0
 	for i := 0; i < len(area.Rows); i++ {
 		currentPosition := [2]int{i * slope.x, i * slope.y}
-		if area.IsTree(currentPosition[0], currentPosition[1]) {
-			nb++
+
+		if currentPosition[1] < area.Height {
+			if area.IsTree(currentPosition[0], currentPosition[1]) {
+				nb++
+			}
 		}
 	}
 	return nb
@@ -60,10 +63,11 @@ type Row struct {
 
 func main() {
 	fmt.Printf("hello")
-	nb := 0
+
 	area := Area{
-		Rows:  []Row{},
-		Width: 0,
+		Rows:   []Row{},
+		Width:  0,
+		Height: 0,
 	}
 
 	fileHandle, err := os.Open("day3.txt")
@@ -73,15 +77,31 @@ func main() {
 	defer fileHandle.Close()
 	fileScanner := bufio.NewScanner(fileHandle)
 
+	nbrows := 0
 	for fileScanner.Scan() {
 		s := fileScanner.Text()
 		area.Width = len(s)
 		area.AddRow(s)
+		nbrows++
 	}
+	area.Height = nbrows
 
-	slope := Slope{x: 3, y: 1}
+	slope1 := Slope{x: 1, y: 1}
+	nb1 := area.HowManyTrees(slope1)
+	fmt.Printf("Number of trees: %d\n", nb1)
+	slope2 := Slope{x: 3, y: 1}
+	nb2 := area.HowManyTrees(slope2)
+	fmt.Printf("Number of trees: %d\n", nb2)
+	slope3 := Slope{x: 5, y: 1}
+	nb3 := area.HowManyTrees(slope3)
+	fmt.Printf("Number of trees: %d\n", nb3)
+	slope4 := Slope{x: 7, y: 1}
+	nb4 := area.HowManyTrees(slope4)
+	fmt.Printf("Number of trees: %d\n", nb4)
+	slope5 := Slope{x: 1, y: 2}
+	nb5 := area.HowManyTrees(slope5)
+	fmt.Printf("Number of trees: %d\n", nb5)
 
-	nb = area.HowManyTrees(slope)
+	fmt.Printf("Total: %d\n", nb1*nb2*nb3*nb4*nb5)
 
-	fmt.Printf("Number of trees: %d\n", nb)
 }
