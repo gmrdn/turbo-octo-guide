b9788775f4ece6c029b38629d851191d802f81b4
Guillaume Rondon <gr@prixtel.com>
2020-12-04 22:46:28 +0100

    Day 4 part 2 with interfaces (kind of polymorphism)

--- src/passport_processing/passport.go
+++ src/passport_processing/passport.go
@@ -5,18 +5,10 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
+	"strconv"
 	"strings"
 )
-
-var MandatoryFields = []string{
-	"byr",
-	"iyr",
-	"eyr",
-	"hgt",
-	"hcl",
-	"ecl",
-	"pid",
-}
 
 type BatchFile struct {
 	Passports []Passport
@@ -35,11 +27,64 @@ func (p *Passport) AddFields(s string) []Field {
 	keyValues := strings.Split(s, " ")
 	for _, kv := range keyValues {
 		kvArray := strings.Split(kv, ":")
-		f := Field{
-			Key:   kvArray[0],
-			Value: kvArray[1],
+		switch kvArray[0] {
+		case "byr":
+			f := Field{
+				Key:       kvArray[0],
+				Value:     kvArray[1],
+				Validator: ByrValidator{},
+			}
+			p.Fields = append(p.Fields, f)
+		case "iyr":
+			f := Field{
+				Key:       kvArray[0],
+				Value:     kvArray[1],
+				Validator: IyrValidator{},
+			}
+			p.Fields = append(p.Fields, f)
+		case "eyr":
+			f := Field{
+				Key:       kvArray[0],
+				Value:     kvArray[1],
+				Validator: EyrValidator{},
+			}
+			p.Fields = append(p.Fields, f)
+		case "hgt":
+			f := Field{
+				Key:       kvArray[0],
+				Value:     kvArray[1],
+				Validator: HgtValidator{},
+			}
+			p.Fields = append(p.Fields, f)
+		case "hcl":
+			f := Field{
+				Key:       kvArray[0],
+				Value:     kvArray[1],
+				Validator: HclValidator{},
+			}
+			p.Fields = append(p.Fields, f)
+		case "ecl":
+			f := Field{
+				Key:       kvArray[0],
+				Value:     kvArray[1],
+				Validator: EclValidator{},
+			}
+			p.Fields = append(p.Fields, f)
+		case "pid":
+			f := Field{
+				Key:       kvArray[0],
+				Value:     kvArray[1],
+				Validator: PidValidator{},
+			}
+			p.Fields = append(p.Fields, f)
+		default:
+			f := Field{
+				Key:       kvArray[0],
+				Value:     kvArray[1],
+				Validator: DefaultValidator{},
+			}
+			p.Fields = append(p.Fields, f)
 		}
-		p.Fields = append(p.Fields, f)
 	}
 	return p.Fields
 }
@@ -48,13 +93,15 @@ func (p *Passport) IsValid() bool {
 	valid := true
 	for _, req := range MandatoryFields {
 		found := false
-		for _, f := range p.Fields {
+		fieldIndex := 0
+		for i, f := range p.Fields {
 			if f.Key == req {
 				found = true
+				fieldIndex = i
 				break
 			}
 		}
-		if found == false {
+		if !found || !p.Fields[fieldIndex].Validator.IsValid(p.Fields[fieldIndex].Value) {
 			valid = false
 			break
 		}
@@ -62,9 +109,108 @@ func (p *Passport) IsValid() bool {
 	return valid
 }
 
+type FieldValidator interface {
+	IsValid(value string) bool
+}
 type Field struct {
-	Key   string
-	Value string
+	Key       string
+	Value     string
+	Validator FieldValidator
+}
+
+var MandatoryFields = []string{
+	"byr",
+	"iyr",
+	"eyr",
+	"hgt",
+	"hcl",
+	"ecl",
+	"pid",
+}
+
+type DefaultValidator struct{}
+
+func (dv DefaultValidator) IsValid(value string) bool {
+	return true
+}
+
+type ByrValidator struct{}
+
+func (v ByrValidator) IsValid(value string) bool {
+	s, err := strconv.Atoi(value)
+	if err == nil && s >= 1920 && s <= 2002 {
+		return true
+	}
+	return false
+}
+
+type IyrValidator struct{}
+
+func (v IyrValidator) IsValid(value string) bool {
+	s, err := strconv.Atoi(value)
+	if err == nil && s >= 2010 && s <= 2020 {
+		return true
+	}
+	return false
+}
+
+type EyrValidator struct{}
+
+func (v EyrValidator) IsValid(value string) bool {
+	s, err := strconv.Atoi(value)
+	if err == nil && s >= 2020 && s <= 2030 {
+		return true
+	}
+	return false
+}
+
+type HgtValidator struct{}
+
+func (v HgtValidator) IsValid(value string) bool {
+	unit := value[len(value)-2:]
+	s, err := strconv.Atoi(value[:len(value)-2])
+	if unit == "cm" {
+		if err == nil && s >= 150 && s <= 193 {
+			return true
+		}
+	} else if unit == "in" {
+		if err == nil && s >= 59 && s <= 76 {
+			return true
+		}
+	}
+	return false
+}
+
+type HclValidator struct{}
+
+func (v HclValidator) IsValid(value string) bool {
+	matched, err := regexp.MatchString(`^#[a-f0-9]{6}$`, value)
+	if err == nil && matched {
+		return true
+	}
+	return false
+}
+
+type EclValidator struct{}
+
+func (v EclValidator) IsValid(value string) bool {
+	valid := []string{"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}
+	for _, expected := range valid {
+		if value == expected {
+			return true
+		}
+	}
+	return false
+}
+
+type PidValidator struct{}
+
+func (v PidValidator) IsValid(value string) bool {
+	matched, err := regexp.MatchString(`^[0-9]{9}$`, value)
+	if err == nil && matched {
+		return true
+	}
+	return false
 }
 
 func main() {
--- src/passport_processing/passport_test.go
+++ src/passport_processing/passport_test.go
@@ -37,35 +37,85 @@ func InitBatchFile() BatchFile {
 	return b
 }
 
-func TestIsValid(t *testing.T) {
-
-	b := InitBatchFile()
-
-	want := true
-	got := b.Passports[0].IsValid()
-	if got != want {
-		t.Errorf("got: %t, instead of: %t.", got, want)
+func InitInvalidPassowrds() BatchFile {
+	b := BatchFile{
+		Passports: []Passport{},
 	}
+	b.AddPassport(Passport{
+		Fields: []Field{},
+	})
+	b.AddPassport(Passport{
+		Fields: []Field{},
+	})
+	b.AddPassport(Passport{
+		Fields: []Field{},
+	})
+	b.AddPassport(Passport{
+		Fields: []Field{},
+	})
+	b.Passports[0].AddFields("eyr:1972 cid:100")
+	b.Passports[0].AddFields("hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926")
+	b.Passports[1].AddFields("iyr:2019")
+	b.Passports[1].AddFields("hcl:#602927 eyr:1967 hgt:170cm")
+	b.Passports[1].AddFields("ecl:grn pid:012533040 byr:1946")
+	b.Passports[2].AddFields("hcl:dab227 iyr:2012")
+	b.Passports[2].AddFields("ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277")
+	b.Passports[3].AddFields("hgt:59cm ecl:zzz")
+	b.Passports[3].AddFields("eyr:2038 hcl:74454a iyr:2023")
+	b.Passports[3].AddFields("pid:3556412378 byr:2007")
+
+	return b
 }
 
-func TestIsNotValid(t *testing.T) {
+func InitValidPassowrds() BatchFile {
+	b := BatchFile{
+		Passports: []Passport{},
+	}
+	b.AddPassport(Passport{
+		Fields: []Field{},
+	})
+	b.AddPassport(Passport{
+		Fields: []Field{},
+	})
+	b.AddPassport(Passport{
+		Fields: []Field{},
+	})
+	b.AddPassport(Passport{
+		Fields: []Field{},
+	})
 
-	b := InitBatchFile()
+	b.Passports[0].AddFields("pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980")
+	b.Passports[0].AddFields("hcl:#623a2f")
+	b.Passports[1].AddFields("eyr:2029 ecl:blu cid:129 byr:1989")
+	b.Passports[1].AddFields("iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm")
+	b.Passports[2].AddFields("hcl:#888785")
+	b.Passports[2].AddFields("hgt:164cm byr:2001 iyr:2015 cid:88")
+	b.Passports[2].AddFields("pid:545766238 ecl:hzl")
+	b.Passports[2].AddFields("eyr:2022")
+	b.Passports[3].AddFields("iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719")
+	return b
+}
+
+func TestAreAllInvalid(t *testing.T) {
+	b := InitInvalidPassowrds()
 
 	want := false
-	got := b.Passports[1].IsValid()
-	if got != want {
-		t.Errorf("got: %t, instead of: %t.", got, want)
+	for _, p := range b.Passports {
+		got := p.IsValid()
+		if got != want {
+			t.Errorf("got: %t, instead of: %t.", got, want)
+		}
 	}
 }
 
-func TestIsValidEvenWithoutCID(t *testing.T) {
-
-	b := InitBatchFile()
+func TestAreAllValid(t *testing.T) {
+	b := InitValidPassowrds()
 
 	want := true
-	got := b.Passports[2].IsValid()
-	if got != want {
-		t.Errorf("got: %t, instead of: %t.", got, want)
+	for _, p := range b.Passports {
+		got := p.IsValid()
+		if got != want {
+			t.Errorf("got: %t, instead of: %t.", got, want)
+		}
 	}
 }
